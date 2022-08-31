8535afe8b38cf4ea01e73e7febfc8390206ee017
Daniil Maltsev <danil.zlatoust9999@gmail.com>
2022-08-31 12:39:29 +0500

    Film-Genre link

--- database/migrations/2022_08_31_060126_create_films_table.php
+++ database/migrations/2022_08_31_060126_create_films_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('films', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 255);
+            $table->bigInteger('genre_id');
+            $table->foreign('genre_id')->references('id')->on('genres');
             $table->timestamps();
         });
     }
--- database/migrations/2022_08_31_060203_create_genres_table.php
+++ database/migrations/2022_08_31_060203_create_genres_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('genres', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 255);
             $table->timestamps();
         });
     }
